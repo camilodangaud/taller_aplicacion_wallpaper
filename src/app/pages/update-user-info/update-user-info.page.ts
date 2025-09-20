71d9c6e0af28e51d75ae21ea70591b5521992d82
camilodangaud <143123036+camilodangaud@users.noreply.github.com>
2025-09-20 11:28:09 -0500

    extenciones

---
+++ src/app/pages/update-user-info/update-user-info.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-user-info',
+  templateUrl: './update-user-info.page.html',
+  styleUrls: ['./update-user-info.page.scss'],
+})
+export class UpdateUserInfoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
